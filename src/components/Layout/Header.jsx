1f63c3c21d1fd73ebfcf4b6cedf4a2e577f810f3
Ayan Khan <beaayank@gmail.com>
2025-06-13 05:53:36 +0530

    Admin-side completed

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { useDispatch, } from "react-redux";
+import { Link } from "react-router-dom";
+import { authActions } from "../../store/authSlice";
+import { useNavigate } from 'react-router-dom';
+
+
+const Header = () => {
+  const dispatch=useDispatch()
+
+  const navigate=useNavigate()
+
+  const logoutHandler=()=>{
+navigate('/')
+dispatch(authActions.logout())
+  }
+
+
+  return (
+    <Navbar bg="secondary" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/home">
+          AdaptHome Admin
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="admin-navbar-nav" />
+        <Navbar.Collapse id="admin-navbar-nav">
+          <Nav className="me-auto">
+            <>
+              <Nav.Link as={Link} to="/product">
+                Product
+              </Nav.Link>
+              <Nav.Link as={Link} to="/category">
+                Category
+              </Nav.Link>
+              <Nav.Link as={Link} to="/orders">
+                Orders
+              </Nav.Link>
+            </>
+          </Nav>
+          
+          <Button onClick={logoutHandler}
+            variant="outline-light"
+          >
+            Logout
+          </Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Header
---
+++ src/components/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const HomePage = () => {
+  return (
+    <Container>
+        <h3 className='mt-3'>You are on Admin Page  </h3>
+    </Container>
+  )
+}
+
+export default HomePage
